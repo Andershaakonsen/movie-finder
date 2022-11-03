6bec739b7bf893da5183b61a4c5b6ea7c3ff0305
Andershaakonsen <64076839+Andershaakonsen@users.noreply.github.com>
2022-11-03 21:38:47 +0100

    fixed typo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Home from "./components/Home";
-import NavBar from "./components/NavBar";
+import NavBar from "./components/Navbar";
 import { Route, Routes } from "react-router-dom";
 import Favorites from "./components/Favorites";
 import Watchlist from "./components/Watchlist";
